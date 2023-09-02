b9c5ada78b66d5b85b99897f2e2e39e80a4f23aa
gingerninja412 <samdpeevor@gmail.com>
2023-09-02 15:51:53 +0100

    built the grid organisation system

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-
-
+const sizeSelector = document.getElementById("size-change")
+const indicator = document.getElementById("sizeIndicator")
+const grid = document.getElementById("grid")
 function fillGrid(size){
+    while(grid.lastChild){
+        grid.removeChild(grid.lastChild)
+    }
     let tileWidth = 800/size
+    tileWidth = tileWidth
     console.log(tileWidth)
     let amountOfTiles = size*size
-    let grid = document.getElementById("grid")
     let tileList = []
     while(tileList.length != amountOfTiles){
         let newTile = document.createElement("div")
@@ -15,4 +19,10 @@ function fillGrid(size){
     }
 }
 
-fillGrid(2)
+function changeIndicator(){
+    let sizeValue = sizeSelector.value
+    console.log(sizeValue)
+    fillGrid(sizeValue)
+    indicator.textContent = `${sizeValue} X ${sizeValue}`
+}
+
